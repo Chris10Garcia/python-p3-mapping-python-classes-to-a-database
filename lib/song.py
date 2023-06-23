a86e369813c187ac49b84c76f2072b0d67ee88a7
Christian Garcia <bones192nd@gmail.com>
2023-06-23 13:04:45 -0400

    interesting lab

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,59 @@
 from config import CONN, CURSOR
+import sqlite3
+
+CONN = sqlite3.connect('music.db')
+CURSOR = CONN.cursor()
 
 class Song:
-    pass
+
+    all = []
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+        self.add_to_all_songs(self)
+
+    @classmethod
+    def add_to_all_songs(cls, song):
+        cls.all.append(song)
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            );
+        """
+
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        CURSOR.execute(sql, (self.name, self.album))
+        CONN.commit()
+
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
+
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
+
+# hello = Song("Hello", 25)
+# hello.save()
+
+# despacito = Song ("Despacito", "Vida")
+# despacito.save()
+
+# songs = CURSOR.execute('SELECT * FROM songs')
+
+# for row in songs:
+#     print(row)
